33da574bd7f37b6ce44342db61e2e2cb9067936c
Hanjun Kim <hallazzang@gmail.com>
2020-08-18 16:52:38 +0900

    Updates

---
+++ precache-manifest.400b8a4b0fc4ad264e617b3d27dc3bd2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51dd9daf6dac06a654cb39b5b94e001",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91be228ee1ae412e69e9",
+    "url": "/static/css/main.91791a15.chunk.css"
+  },
+  {
+    "revision": "5d5aabd6f57cea283bf6",
+    "url": "/static/js/2.226e04b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.226e04b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91be228ee1ae412e69e9",
+    "url": "/static/js/main.bd63b6b4.chunk.js"
+  },
+  {
+    "revision": "dfe3d817a9ac0c05d2c9",
+    "url": "/static/js/runtime-main.84cfcdfc.js"
+  },
+  {
+    "revision": "dd47e048dabeae0184f14e4df9d2ba5d",
+    "url": "/static/media/space.dd47e048.jpg"
+  },
+  {
+    "revision": "383fcde4e6b6dd65cbf423f99b4fa94b",
+    "url": "/static/media/yhat-logo.383fcde4.png"
+  }
+]);
